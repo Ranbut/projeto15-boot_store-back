2ef25062170273cb43457c2ae0fa827b98a93fbe
ana-lapas <anapaulalapas@gmail.com>
2023-01-26 19:13:06 -0400

    Feat: Função de SignUp

--- src/middlewares/auth.Middleware.js
+++ src/middlewares/auth.Middleware.js
@@ -1,24 +1,29 @@
 import { signUpSchema } from "../schemas/signUp.Schema.js";
 import { usersCollection } from "../config/db.js";
 
-export async function SignUpVerification (req, res, next){
+export async function SignUpVerification(req, res, next) {
     const user = req.body;
-    console.log("User no middleware, que veio no body")
-    console.log(user)
-    
-    try{
-        const { error } = signUpSchema.validate(user, { abortearly : false});
+
+    try {
+
+        const { error } = signUpSchema.validate(user, { abortearly: false });
+
         if (error) {
             const errorMessage = error.details.map(detail => detail.message);
             return res.status(400).send(errorMessage);
-        }
-        const verifyUser = await usersCollection.findOne({email: user.email});
-        if(verifyUser){
+        };
+
+        const verifyUser = await usersCollection.findOne({ email: user.email });
+
+        if (verifyUser) {
             return res.status(409).send("Este usuário já existe, faça login");
         }
-    } catch(err){
-        console.log(err)
-        return res.status(500).send("Ocorreu um erro, tente novamente mais tarde")
+        
+        res.locals.user = user;
+
+    } catch (err) {
+        console.log(err);
+        return res.status(500).send("Ocorreu um erro, tente novamente mais tarde");
     }
     next();
 }
